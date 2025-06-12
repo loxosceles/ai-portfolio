6c01672bf03846b2bcdd1888018090715465367a
Magnus Henkel <dev@loxosceles.me>
2025-06-12 22:26:03 +0000

    Move frontend code into frontend sub-folder

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import GraphQLProvider from "@/components/amplify-provider";
+import GraphQLProvider from "@/components/apollo-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
