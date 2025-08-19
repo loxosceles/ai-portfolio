7f988449e9d75d3cc3ab212116af3f350c0fd602
Magnus Henkel <dev@loxosceles.me>
2025-08-19 15:09:54 +0000

    Refine brand icon

--- frontend/components/icons/parentheses-initials-icon.tsx
+++ frontend/components/icons/parentheses-initials-icon.tsx
@@ -10,26 +10,26 @@ export default function ParenthesesInitialsIcon({
       <path
         d="M20 15 C5 20, 5 80, 20 85"
         stroke="currentColor"
-        strokeWidth="12"
+        strokeWidth="10"
         strokeLinecap="round"
       />
       <path
         d="M80 15 C95 20, 95 80, 80 85"
         stroke="currentColor"
-        strokeWidth="12"
+        strokeWidth="10"
         strokeLinecap="round"
       />
       <path
         d="M30 75 L30 25 L42 55 L58 25 L58 75"
         stroke="currentColor"
-        strokeWidth="8"
+        strokeWidth="6"
         strokeLinecap="round"
         fill="none"
       />
       <path
         d="M62 25 L62 75 M62 50 L85 50 M85 25 L85 75"
         stroke="currentColor"
-        strokeWidth="8"
+        strokeWidth="6"
         strokeLinecap="round"
       />
     </svg>
