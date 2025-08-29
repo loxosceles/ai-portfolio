887c8af7ea04f25997d119808bd73410cfdb5dae
Magnus Henkel <dev@loxosceles.me>
2025-08-29 16:07:51 +0000

    Update admin configs to with new path structure

--- infrastructure/admin/lib/config.mjs
+++ infrastructure/admin/lib/config.mjs
@@ -37,7 +37,7 @@ const ADMIN_CONFIG = {
   paths: {
     ssmTemplate: '/portfolio/{stage}/{paramName}',
     dataTemplate: 'data',
-    schemaTemplate: 'schemas/{schemaFile}',
+    schemaTemplate: 'data/{stage}/schemas/{schemaFile}',
     stateFile: 'data/.admin-state.json'
   },
 
